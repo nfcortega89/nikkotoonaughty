66675bd37aacf7ee11cf861b2b8d64ce4ad1e2ad
Nikko Ortega <NFCOrtega89@gmail.com>
2017-02-23 11:44:07 -0800

    rendering request images

--- src/components/card.js
+++ src/components/card.js
@@ -1,26 +1,28 @@
 import React, { Component } from 'react';
 import jsonp from 'jsonp';
-
-export default function Card() {
-  const ACCESS_TOKEN = window.location.hash.split('=')[1];
-  const url = `https://api.instagram.com/v1/users/self/media/recent?access_token=${ACCESS_TOKEN}`;
+const ACCESS_TOKEN = window.location.hash.split('=')[1];
+const url = `https://api.instagram.com/v1/users/self/media/recent?access_token=${ACCESS_TOKEN}`;
 
 jsonp(url, null, function (err, data) {
-    if (err) {
-      console.error(err.message);
-    }
-    else {
-        let USER_PICS = data.data;
-        console.log(USER_PICS);
-        let pics = [];
-        USER_PICS.map((picture, index) => {
-          let pic = <img src={picture.images.standard_resolution.url} />
-          pics.push(pic);
-        })
-        return pics;
-    }
-  });
-  return (
-    <div>HI</div>
-  )
+  if (err) {
+    console.error(err.message);
+  }
+  else {
+      let USER_PICS = data.data;
+      console.log(USER_PICS);
+      let pics = [];
+      USER_PICS.map((picture, index) => {
+        let pic = <img src={picture.images.standard_resolution.url} />
+        return pics.push(pic);
+        console.log('a', pics);
+      })
+  }
+});
+export default class Card extends Component {
+  render() {
+    return (
+      <div>Hi</div>
+    )
+  }
+
 }
