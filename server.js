6d631ae22b5dcdd1e1c1b6d027f526fd9d43b78e
Nikko Ortega <NFCOrtega89@gmail.com>
2017-03-07 21:30:24 -0800

    updated port

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const jsonParser = bodyParser.json();
+
+const app = express();
+
+app.use(express.static('public'))
+app.use('/assets', express.static('assets'))
+app.use(jsonParser)
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './index.html'))
+})
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log("Listening on port 8080")
+})
