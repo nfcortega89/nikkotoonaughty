5953497203545116039ae69e186b9158667a9d68
Nikko Ortega <NFCOrtega89@gmail.com>
2017-03-09 20:18:20 -0800

    moved index.html

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ app.use('/assets', express.static('assets'))
 app.use('/style', express.static('style'))
 app.use(jsonParser)
 
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './index.html'))
-})
+// app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, './index.html'))
+// })
 
 app.listen(process.env.PORT || 8080, () => {
   console.log("Listening on port 8080")
