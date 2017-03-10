716e0abe31ce673be360a33eddd2659a26016877
Nikko Ortega <NFCOrtega89@gmail.com>
2017-03-09 20:26:26 -0800

    node server.js

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require('express');
+var spa = require('express-spa')
 const bodyParser = require('body-parser');
 const path = require('path');
 
 const jsonParser = bodyParser.json();
 
 const app = express();
-
+app.use(jsonParser)
+app.use(spa())
 app.use(express.static('public'))
 app.use('/assets', express.static('assets'))
 app.use('/style', express.static('style'))
-app.use(jsonParser)
+
 
 // app.get('*', (req, res) => {
 //     res.sendFile(path.resolve(__dirname, './index.html'))
